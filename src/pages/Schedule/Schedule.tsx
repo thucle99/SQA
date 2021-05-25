d1810fa829127e9e1a0a78d17f3d16433c376bb0
phamtienanh157 <phamtienanh157@gmail.com>
2021-05-25 21:58:44 +0700

    create table

--- src/pages/Schedule/Schedule.tsx
+++ src/pages/Schedule/Schedule.tsx
@@ -1,30 +1,65 @@
 import React from "react"
+import { Table } from "antd"
 import { connect, ConnectedProps } from "react-redux"
 import MainLayout from "src/layouts/MainLayout"
-import styles from "./Schedule.module.scss"
+import TableRow from "src/pages/Schedule/TableRow/TableRow"
+import "./Schedule.module.scss"
 
-const mapStateToProps = (state: AppState) => ({
-  
-})
+const mapStateToProps = (state: AppState) => ({})
 
-const mapDispatchToProps = {
-  
-}
+const mapDispatchToProps = {}
 
 const connector = connect(mapStateToProps, mapDispatchToProps)
 
 interface Props extends ConnectedProps<typeof connector> {}
 const Schedule = (props: Props) => {
-  const {
-    
-  } = props
- 
+  const {} = props
   return (
-    <MainLayout>
-      <div className={styles.content}>
-        <p>chao anh em</p>
-      </div>
-    </MainLayout>
+    <div>
+      <MainLayout>he</MainLayout>
+      <table>
+        <thead>
+          <tr>
+            <td></td>
+            <td>THỨ HAI</td>
+            <td>THỨ BA</td>
+            <td>THỨ TƯ</td>
+            <td>THỨ NĂM</td>
+            <td>THỨ SÁU</td>
+            <td>THỨ BẢY</td>
+            <td>CHỦ NHẬT</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          <TableRow title="Tiết 1" />
+          <TableRow title="Tiết 2" />
+          <TableRow title="Tiết 3" />
+          <TableRow title="Tiết 4" />
+          <TableRow title="Tiết 5" />
+          <TableRow title="Tiết 6" />
+          <TableRow title="Tiết 7" />
+          <TableRow title="Tiết 8" />
+          <TableRow title="Tiết 9" />
+          <TableRow title="Tiết 10" />
+          <TableRow title="Tiết 11" />
+          <TableRow title="Tiết 12" />
+        </tbody>
+        <thead>
+          <tr>
+            <td></td>
+            <td>THỨ HAI</td>
+            <td>THỨ BA</td>
+            <td>THỨ TƯ</td>
+            <td>THỨ NĂM</td>
+            <td>THỨ SÁU</td>
+            <td>THỨ BẢY</td>
+            <td>CHỦ NHẬT</td>
+            <td></td>
+          </tr>
+        </thead>
+      </table>
+    </div>
   )
 }
 
---
+++ src/pages/Schedule/TableRow/TableRow.tsx
@@ -0,0 +1,16 @@
+import "./TableRow.module.scss"
+export default function TableRow(props) {
+  return (
+    <tr>
+      <td>{props.title}</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>{props.title}</td>
+    </tr>
+  )
+}
