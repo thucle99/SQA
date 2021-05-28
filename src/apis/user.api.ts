dd0afe580977d828d56c7f0d0d4e7fb233484163
Thuc <thucleit99@gmail.com>
2021-05-28 17:30:36 +0700

    config bug

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -1,26 +1,18 @@
-import axios from "axios"
 export const loginApi = ({
   username,
   password
 }: ReqLogin): Promise<ResLoginApi> =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
-      axios
-        .post("http://localhost:8080/login", {
-          username,
-          password
+      if (username === "admin" && password === "123") {
+        resolve({
+          data: {
+            access_token: "82jdu82193yh90sad83hxfgsd"
+          },
+          message: "Login thành công"
         })
-        .then(res => {
-          localStorage.setItem("username", username)
-          resolve({
-            data: {
-              access_token: res.data.accessToken
-            },
-            message: "Login thành công"
-          })
-        })
-        .catch(function (error) {
-          reject(new Error("Tài khoản hoặc mật khẩu không chính xác"))
-        })
+      } else {
+        reject(new Error("Login thất bại"))
+      }
     }, 100)
   })
--- src/pages/Schedule/Schedule.thunks.ts
+++
@@ -1,9 +0,0 @@
-import { getRegistrationListApi } from "src/apis/registrationList.api"
-import * as actions from "./Schedule.action"
-
-export const getRegistrationList = () => dispatch => {
-  dispatch(actions.registrationListRoomRequested())
-  return getRegistrationListApi()
-    .then(res => dispatch(actions.registerListRoomSuccess(res)))
-    .catch(err => Promise.reject(dispatch(actions.getItemFailed(err))))
-}
