38f6d4025c3c20e3f7e3dc3620f3464f7d4f2941
Thuc <thucleit99@gmail.com>
2021-05-28 17:42:41 +0700

    nothing

--- src/pages/Schedule/TableRow/TableRow.tsx
+++ src/pages/Schedule/TableRow/TableRow.tsx
@@ -1,13 +1,15 @@
 import "./TableRow.module.scss"
 export default function TableRow(props) {
+  const { lesson } = props
   return (
     <tr>
       <td>{props.title}</td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      {lesson[2] && <td>{lesson[2].ten}</td>}
+      {lesson[3] && <td>{lesson[3].ten}</td>}
+      {lesson[4] && <td>{lesson[4].ten}</td>}
+      {lesson[5] && <td>{lesson[5].ten}</td>}
+      {lesson[6] && <td>{lesson[6].ten}</td>}
+      {lesson[7] && <td>{lesson[7].ten}</td>}
       <td></td>
       <td></td>
       <td>{props.title}</td>
